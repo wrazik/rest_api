7a898a09f56d0c97f4d9ca29eeef0941fea38f2f
wrazik <wojciech.razik@gmail.com>
2025-01-31 11:19:55 +0100

    Cleanup main

--- src/rest_api.cpp
+++ src/rest_api.cpp
@@ -1,27 +1,11 @@
 #include <algorithm>
-#include <boost/asio/dispatch.hpp>
-#include <boost/asio/strand.hpp>
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
-#include <boost/config.hpp>
-#include <cstdlib>
-#include <functional>
 #include <iostream>
-#include <memory>
-#include <string>
-#include <thread>
-#include <vector>
+#include <cstdlib>
 
 #include "server.hpp"
 
-namespace beast = boost::beast;    // from <boost/beast.hpp>
-namespace http = beast::http;      // from <boost/beast/http.hpp>
-namespace net = boost::asio;       // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;  // from <boost/asio/ip/tcp.hpp>
 
 int main(int argc, char* argv[]) {
-    // Check command line arguments.
     if (argc != 5) {
         std::cerr << "Usage: http-server-async <address> <port> <doc_root> "
                      "<threads>\n"
